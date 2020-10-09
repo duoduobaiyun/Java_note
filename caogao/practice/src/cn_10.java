384e265a14fb43837cdacd256bb4349c6dbccc09
liulianwanfan <3557452792@qq.com>
2020-10-09 23:39:13 +0800

    优化方法

--- caogao/practice/src/cn_10.java
+++ caogao/practice/src/cn_10.java
@@ -1,10 +1,18 @@
 public class cn_10 {
     public static void main(String[] args) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < 9; i++) {//行
+//            for (int j = 0; j < i; j++) {//列
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+       for (int i =0;i<7;i++){
+           for (int j =0;j<7-i;j++){//因为7-0=7所以要打印7次列,就是在一行上打印7次*,还有一件事得注意,初始化得算一次,
+                                    //也就是i=0本身也算一次,因为要算在j里面
+             System.out.print("*");
+           }
+           System.out.println();
+       }
     }
 }
---
+++ caogao/practice/src/cn_11.java
@@ -0,0 +1,38 @@
+/*
+* 100以内的所有质数的输出。
+* 质数:素数,只能被1和它本身整除的自然数。--从2开始,到这个数-1结束为止,都不能被这个数本身整除
+*
+* 最小的质数是:2*/
+
+
+public class cn_11 {
+    public static void main(String[] args){
+        //获取当前时间距离1970-01-01 00:00:00 的毫秒数
+        long start =System.currentTimeMillis();
+
+        boolean isFlag=true;//标识i是否被除尽,一旦除尽,修改其他值
+        int count=0;//记录质数的个数   优化四
+
+        for(int i=2;i<=100000;i++){//遍历100000以内的自然数
+            //boolean isFlag=true;//标识i是否被除尽,一旦除尽,修改其他值
+            for (int j=2;j<Math.sqrt(i);j++){//j:被i去除 优化三
+                if (i%j==0){//i被j除尽
+                    isFlag =false;
+                    break;//优化一:只对本身非质数的自然数是有效的
+                    //System.out.println(i);
+                }
+            }
+            //
+            if (isFlag==true){
+               //System.out.println(i);
+                count++;
+            }
+            //重置isFlag
+            isFlag=true;
+        }
+        //获取当前时间距离1970-01-01 00:00:00 的毫秒数
+        long end =System.currentTimeMillis();
+        System.out.println("质数的个数为:"+count);
+        System.out.println("所花费的时间为:"+(end-start));
+    }
+}
