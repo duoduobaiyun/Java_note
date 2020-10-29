c5d540a73a2d645734727d8da2dad26c0d004cbe
liulianwanfan <3557452792@qq.com>
2020-10-29 18:30:04 +0800

    递归,复习,面向对象的封装与隐藏

---
+++ caogao/practice/src/cn_57.java
@@ -0,0 +1,13 @@
+public class cn_57 {
+    public static void main(String[] args) {
+        String s1 ="hello";
+
+        cn_57 test = new cn_57();
+        test.change(s1);
+        System.out.println(s1);
+    }
+    public  void  change(String s){
+        s="hi~~~";
+        System.out.println(s);
+    }
+}
