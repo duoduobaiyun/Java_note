41083950702298e60eeda8b7d4f231b3fde2ebf3
liulianwanfan <3557452792@qq.com>
2020-10-29 15:01:47 +0800

    新增的递归题目

--- caogao/practice/src/cn_54.java
+++ caogao/practice/src/cn_54.java
@@ -14,11 +14,22 @@ public class cn_54 {
         //方式二:
         cn_54 test = new cn_54();
         //和
-        int sum1 = test.getSum(5);
+        int sum1 = test.getSum(100);
         System.out.println(sum1);
+
+        System.out.println("*************");
+
         //乘积
-        int sum2 = test.getSum1(5);
+        int sum2 = test.getSum1(100);
         System.out.println(sum2);
+
+        System.out.println("**************");
+
+        /*第一种方法*/
+//        int value =test.f(10);
+//        System.out.println(value);
+
+        //System.out.println( test.f(10));//第二种方法
     }
 
     //例1:计算1-n之间所有自然数的和
@@ -38,4 +49,18 @@ public class cn_54 {
             return n * getSum(n - 1);
         }
     }
+
+    //例3:已知有一个数列:f(0)=1,f(1)=4,f(n+2)=2*f(n+1)+f(n),
+    //其中n是大于0的整数,求f(10)的值。
+    public int f(int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 4;
+        } else {
+            //return f(n + 2)-2*f(n+1);
+            return 2 * f(n - 1) + f(n - 2);//把f(n+2)当作f(n)就可以算出
+        }
+    }
+
 }
